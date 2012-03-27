9f1212edc971143ec109895e2e3b04bcb8e55066
Avi Goldberg <aviindub@gmail.com>
2012-03-27 15:53:20 -0400

    working version with full stream and updating of tracked words

--- app/twitter.js
+++ app/twitter.js
@@ -1,8 +1,9 @@
 var twitter = require('ntwitter');
 var redis = require('redis');
-var credentials = require('./credentials.js'); //read credentials from file
-//create redis client
+var credentials = require('./credentials.js');
 var client = redis.createClient(); //using local redis server, no arguments required
+var trackedWords;
+
 var twitter = new twitter({
 //use credentials from credentials.js
 consumer_key: credentials.consumer_key,
@@ -12,33 +13,34 @@ consumer_key: credentials.consumer_key,
 
 });
 
-
-function tStream(trackedWords) {
-	
+//first get list of tracked words
+client.smembers('words', function (error, result) {
+	if (error) {console.log(error);}
+	trackedWords = result;
+	//start the twitter stream
 	twitter.stream(
-		'statuses/filter', //return statuses that match tracked keywords
-		{track: trackedWords}, //track tweets that contain keywords
+		'statuses/sample', //returns sample of all statuses
 		function(stream){
 			stream.on('data', function(tweet){
-				//first extract the url from the tweet
-				var url = "";
-				try {
-					//test for expanded URLs
-					if (tweet.entities.urls.length > 0) {
-						if (tweet.entities.urls[0].expanded_url != null) {
-							url = tweet.entities.urls[0].expanded_url;
-						}	//test for shortened URLs
-						else if (tweet.entities.urls.length > 0 && tweet.entities.urls[0].url != null) {
-							url = tweet.entities.urls[0].url; 
-						}
-					}
-				} catch (error) {console. log ("error: " + error);}
 				
-				if (url != "") { //if url successfully extracted
-					console.log(url);
-					//loop through array or RegExp to see which word(s) match tweet text
-					for (var i = 0 ; i < trackedWords.length ; i++) {
-						if (tweet.text.match(new RegExp(trackedWords[i]))) {
+				//loop through trackedWords to see which word(s) match tweet text
+				for (var i = 0 ; i < trackedWords.length ; i++) {
+					if (tweet.text.match(new RegExp(trackedWords[i]))) {
+						console.log("matches " + trackedWords[i]);
+						if (trackedWords[i] === "rad") { console.log(tweet.text); }
+						var url = ""; 
+						//extract the url from the tweet...first test for expanded URLs
+						if (tweet.entities.urls.length > 0) {
+							if (tweet.entities.urls[0].expanded_url != null) {
+								url = tweet.entities.urls[0].expanded_url;
+							}	//test for shortened URLs
+							else if (tweet.entities.urls[0].url != null) {
+							url = tweet.entities.urls[0].url; 
+							}
+						}
+						
+						if (url != "") { //if url successfully extracted
+							console.log(trackedWords[i] +" : "+ url);
 							//increment count if match found
 							//also automagically starts new count if needed
 							client.zincrby(trackedWords[i], 1, url);
@@ -46,30 +48,15 @@ function tStream(trackedWords) {
 					}
 				}
 			});
-			
-			setInterval(function () {
-				//get the current list of tracked words and call twitter.stream with it
-				client.smembers('words', function (error, trackedWords) {
-					if (error) {console.log(error);}
-					stream.destroy();
-					tStream(trackedWords);
-				});
-			},5000);	
-			
 		}
 	);
-}
 
-
-function refresh () {
-	//get the current list of tracked words and call twitter.stream with it
-	client.smembers('words', function (error, trackedWords) {
-		if (error) {console.log(error);}
-		tStream(trackedWords);
-	});
-}
-
-client.smembers('words', function (error, trackedWords) {
-	if (error) {console.log(error);}
-	tStream(trackedWords);
+	//every 5 seconds, refresh list of trackedWords
+	setInterval(function () {
+		client.smembers('words', function (error, result) {
+			if (error) {console.log(error);}
+			trackedWords = result;
+			console.log("updated trackedWords");
+		});
+	},5000);
 });
